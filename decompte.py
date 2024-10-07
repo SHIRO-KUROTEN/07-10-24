7c509890aa11e4d69aa939764f8a6dc025c8e135
SHIRO-KUROTEN <johnny.vds@eica.be>
2024-10-07 16:55:21 +0000

    compt

---
+++ decompte.py
@@ -0,0 +1,7 @@
+i = 5  # on initialise i a la valeur 5  , i  prend la valeur 5 il n y est pas egale #
+while i >= 0: # tant que i est superieur  ou egale 0 #
+    print(i) # on afiche i#
+    i = i - 1 # et on le decremente " enlever 1"#
+print("decollage")
+#le wild sgnifie tant que #
+
